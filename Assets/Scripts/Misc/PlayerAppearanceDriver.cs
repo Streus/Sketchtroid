023765628899f89a59e17c10ba570601985bd072
Sam Streed <embersteelstreus@gmail.com>
2017-10-26 16:27:12 -0500

    Playing around with the Appearance driver again. Progress?

--- Assets/Scripts/Misc/PlayerAppearanceDriver.cs
+++ Assets/Scripts/Misc/PlayerAppearanceDriver.cs
@@ -47,17 +47,32 @@ public class PlayerAppearanceDriver : MonoBehaviour
 
 	private void partAdded(Ability a)
 	{
-		
+		partSwapped (a, a, 0);
 	}
 
 	private void partRemoved(Ability a)
 	{
-
+		GameObject[] existing = abilityToPart (a.name);
+		foreach (GameObject g in existing)
+		{
+			Destroy (g);
+		}
 	}
 
 	private void partSwapped(Ability a, Ability b, int index)
 	{
-
+		GameObject pref = Resources.Load<GameObject> (partsPath + a.name);
+		GameObject[] existing = abilityToPart (b.name);
+		for (int i = 0; i < existing.Length; i++)
+		{
+			Destroy (existing [i]);
+			existing [i] = Instantiate (pref, entity.transform, false);
+			if (i == 1)
+				existing [i].transform.localScale = new Vector3 (
+					existing [i].transform.localScale.x,
+					existing [i].transform.localScale.y,
+					-existing [i].transform.localScale.z);
+		}
 	}
 
 	private void dtChanged(DamageType dt)
@@ -72,8 +87,21 @@ public class PlayerAppearanceDriver : MonoBehaviour
 		dtSymbol.color = Bullet.damageTypeToColor (dt);
 	}
 
-	private void setCone(int skinIndex)
+	private GameObject[] abilityToPart(string s)
 	{
-		
+		if (s == "Spray" || s == "Refract" || s == "Lay Waste" || s == "Ricochet")
+		{
+			return new GameObject[]{ cone };
+		}
+		else if (s == "Overdrive" || s == "Propel" || s == "Shift" || s == "Phase")
+		{
+			return new GameObject[]{ leftWing, rightWing };
+		}
+		else if (s == "Displace" || s == "Grapple" || s == "Flash" || s == "Reflect")
+		{
+			return new GameObject[]{ engine };
+		}
+		else
+			return new GameObject[]{ };
 	}
 }
