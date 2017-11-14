492c620a6220fef69ac911531d8db80fef37977b
Sam Streed <embersteelstreus@gmail.com>
2017-11-14 16:55:57 -0600

    Added a jump command to quickly transition between scenes in-game.

---
+++ Assets/Scripts/Testing/Commands/Jump.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Commands
+{
+	public class Jump : Command
+	{
+		public override string getHelp ()
+		{
+			return "Send the game to the indicated room. Usage: jump <room name> [-s]";
+		}
+
+		public override string getInvocation ()
+		{
+			return "jump";
+		}
+
+		public override string execute (params string[] args)
+		{
+			bool saveData = false;
+			string jumpRoom = "";
+
+			jumpRoom = args [1];
+			if (args.Length > 2)
+				saveData = args [2] == "-s";
+
+			if (saveData)
+			{
+				SceneStateManager.instance ().transitionTo (jumpRoom);
+				Console.log.println ("Transitioned to " + jumpRoom, Console.LogTag.info);
+			}
+			else
+			{
+				SceneStateManager.instance ().jumpTo (jumpRoom);
+				Console.log.println ("Jumped to " + jumpRoom, Console.LogTag.info);
+			}
+
+			return "Successfully changed scene!";
+		}
+	}
+}
