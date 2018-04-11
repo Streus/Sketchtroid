4941e6a9219e6d4e5c5635aad755551cfaa1e129
Sam Streed <embersteelstreus@gmail.com>
2018-04-10 20:43:35 -0500

    Fixed issue with pressure plate and multiple occupants.

--- Assets/Scripts/World Elements/Triggers/PressurePlate.cs
+++ Assets/Scripts/World Elements/Triggers/PressurePlate.cs
@@ -4,15 +4,32 @@ using CircuitNodes;
 [RequireComponent(typeof(Collider2D))]
 public class PressurePlate : Toggle
 {
+	private int occupants;
+
+	public void Awake()
+	{
+
+		occupants = 0;
+	}
+
 	public void OnTriggerEnter2D(Collider2D other)
 	{
-		if(other.GetComponent<Bullet>() == null)
+		if (other.GetComponent<Bullet> () == null)
+		{
+			occupants++;
 			setActive (true);
+		}
 	}
 
 	public void OnTriggerExit2D(Collider2D other)
 	{
-		if(other.GetComponent<Bullet>() == null)
-			setActive (false);
+		if (other.GetComponent<Bullet> () == null)
+		{
+			if (--occupants <= 0)
+			{
+				occupants = 0;
+				setActive (false);
+			}
+		}
 	}
 }
